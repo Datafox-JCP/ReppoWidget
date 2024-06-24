301d52c23b2edb6a4188a9f3de65bb23d26e4e48
Datafox-JCP <datafox@outlook.com>
2024-06-23 23:16:29 -0600

    Initial Commit

---
+++ ReppoWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReppoWidget
+//
+//  Created by Juan Hernandez Pazos on 23/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReppoWidget/ReppoWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReppoWidgetApp.swift
+//  ReppoWidget
+//
+//  Created by Juan Hernandez Pazos on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ReppoWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
